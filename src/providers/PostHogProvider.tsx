722ea468afa0589c3b0302b1a13093c07f30fbae
nomnom58 <tranmanhquan.ptit@gmail.com>
2026-04-10 13:44:43 +0700

    đổi favicon, xóa dòng capture_referrer
    xó dòng capture_referrer: true ở file PostHogProvider.tsx

--- src/providers/PostHogProvider.tsx
+++ src/providers/PostHogProvider.tsx
@@ -9,7 +9,6 @@ if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
     capture_pageview: false,
-    capture_referrer: true
   })
 }
 
